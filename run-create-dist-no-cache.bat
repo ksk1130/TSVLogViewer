92be9c5257deb7bcd15a7c27c0de258aec45a13e
ksk1130 <ksk1130@users.noreply.github.com>
2026-02-16 00:12:01 +0900

    Exe化 (#19)
    * Windows用アプリイメージの作成タスクを追加し、バッチファイルを更新
    * アプリにアイコンを追加し、Windowsアプリイメージ作成タスクを更新

--- run-create-dist-no-cache.bat
+++ run-create-dist-no-cache.bat
@@ -1,6 +1,6 @@
 @echo off
-REM Run createWindowsDistributionWithJRE with configuration cache disabled
+REM Run createWindowsDistributionWithJRE and createWindowsAppImage with configuration cache disabled
 setlocal
 cd /d %~dp0
-call gradlew.bat :app:createWindowsDistributionWithJRE --no-configuration-cache --no-daemon %*
+call gradlew.bat :app:createWindowsDistributionWithJRE :app:createWindowsAppImage --no-configuration-cache --no-daemon %*
 endlocal
